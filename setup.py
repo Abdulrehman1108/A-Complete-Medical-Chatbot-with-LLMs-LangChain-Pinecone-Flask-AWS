49bb2b81802bd5cf6db5c2d68f2890478fccc2a3
Abdul Rehman Khan <kamni11811@gmail.com>
2026-04-18 15:05:36 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Abdul Rehman Khan',
+    author_email='kmani11811@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+
+)
+
+   
